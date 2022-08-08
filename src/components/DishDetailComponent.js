b73931faa2c7eb2d5dc68d410efaf9d1a91559c5
mricom <mricomorillo@gmail.com>
2022-08-08 13:30:02 +0200

    Fetch Post Comment.

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -34,7 +34,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
   if (comments.length === 0) {
     return <div></div>;
   } else {
@@ -56,7 +56,7 @@ function RenderComments({ comments, addComment, dishId }) {
             </li>
           ))}
         </List>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
       </>
     );
   }
@@ -80,7 +80,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.dishId,
       values.rating,
       values.author,
@@ -203,7 +203,7 @@ const DishDetail = (props) => {
           <div className="col-12 col-md-5 m-1">
             <RenderComments
               comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               dishId={props.dish.id}
             />
           </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,15 +11,15 @@ import { connect } from "react-redux";
 import { actions } from "react-redux-form";
 
 import {
-  addComment,
+  postComment,
   fetchDishes,
   fetchComments,
   fetchPromos,
 } from "../redux/ActionCreators";
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
@@ -79,7 +79,7 @@ class Main extends Component {
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     };
